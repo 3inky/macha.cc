0a91b4a5824c3629399ad304b71bb1d12d1907ef
rootedguy1 <lexiiboi@proton.me>
2025-12-07 03:46:45 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,46 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Macha.cc",
   description: "Browse and load Roblox scripts and executors easily with Macha.cc!",
-}
+  icons: {
+    icon: "/favicon.ico",
+  },
+
+  themeColor: "#f7c5d8",
+
+  openGraph: {
+    title: "Macha.cc - Roblox Scripts & Executors",
+    description: "Browse and load Roblox scripts and executors easily with Macha.cc!",
+    url: "https://macha.cc",
+    siteName: "Macha.cc",
+    images: [
+      {
+        url: "/images/webimg.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Macha.cc Roblox Preview",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Macha.cc - Roblox Scripts & Executors",
+    description: "Browse and load Roblox scripts and executors easily with Macha.cc!",
+    images: ["/images/webimg.jpg"],
+  },
+
+  keywords: [
+    "Roblox",
+    "Roblox Scripts",
+    "Roblox Executors",
+    "Macha",
+    "Macha.cc",
+    "Script Hub",
+    "Roblox Exploits"
+  ],
+};
+
 
 export default function RootLayout({
   children,
